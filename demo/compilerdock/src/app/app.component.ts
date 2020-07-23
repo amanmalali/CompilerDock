c8cdae1d3f8703ca0fe2f27cc2ea19f692c41037
praveen5959 <praveenbhat98@gmail.com>
2020-07-23 20:48:10 +0530

    Demo Addedd

---
+++ demo/compilerdock/src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'compiler';
+  editorOptions = {theme: 'vs-dark', language: 'javascript'};
+  code: string= '';
+
+  constructor(private http: HttpClient){}
+  stdout:string;
+  submit(){
+    var e=<HTMLSelectElement>document.getElementById('lang');
+    var lang = e.options[e.selectedIndex].value;
+    var stdin = <HTMLTextAreaElement>document.getElementById('stdin-text');
+    var payload={
+      lang: lang,
+      code: this.code,
+      id: "1",
+      stdin: stdin.value
+    };
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json'
+      })
+    };
+    this.http.post('http://13.232.248.211:5000/',payload,httpOptions).subscribe((res:any)=>{
+      this.stdout=res.output;
+      document.getElementById('stdout-text').innerText=this.stdout;
+    });
+    
+  }
+}
---
+++ demo/compilerdock/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MonacoEditorModule } from 'ngx-monaco-editor';
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    MonacoEditorModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
