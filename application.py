fe2c2333934515be37fa8ff22b4f08d5fc4da62f
Mridul Tiwary <tiwary.mridul@gmail.com>
2020-07-28 20:25:00 +0530

    add load_dotenv support

--- application.py
+++ application.py
@@ -1,8 +1,10 @@
+from dotenv import load_dotenv
 from quart import Quart
 from quart_cors import cors
 
 from src.compiler import compiler_bp
 
+load_dotenv(verbose=True)
 app = Quart(__name__)
 cors = cors(app)
 app.config["CORS_HEADERS"] = "Content-Type"
