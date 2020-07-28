2c3dfdf2ca3ec5242a9e5fadd01dd6b91da117f0
Mridul Tiwary <tiwary.mridul@gmail.com>
2020-07-28 20:28:30 +0530

    update test cases

--- tests/integration/test_compiler.py
+++ tests/integration/test_compiler.py
@@ -2,14 +2,33 @@ import pytest
 
 from application import app
 
-data = {"lang": "python3", "code": "print('Hello World')", "stdin": "", "id": 1}
-expected = {"output": "Hello World\n", "error": "", "fail": 0, "timeout": 0, "id": 1}
-
 
 @pytest.mark.asyncio
-async def test_compiler():
+@pytest.mark.parametrize(
+    "inp, out",
+    [
+        (  # successful compilation
+            {"lang": "python3", "code": "print('Hello World')", "stdin": "", "id": 1},
+            (
+                {
+                    "output": "Hello World\n",
+                    "error": "",
+                    "fail": False,
+                    "timeout": False,
+                    "id": 1,
+                },
+                200,
+            ),
+        ),
+        (  # unsupported language input
+            {"lang": "pytho", "code": "print('Hello World')", "stdin": "", "id": 1},
+            ({"error": "Unknown or unsupported language pytho"}, 400,),
+        ),
+    ],
+)
+async def test_compiler(inp, out):
     test_client = app.test_client()
-    reponse = await test_client.post("/", json=data)
+    reponse = await test_client.post("/", json=inp)
     result = await reponse.get_json()
-    assert reponse.status_code == 200
-    assert result == expected
+    assert reponse.status_code == out[1]
+    assert result == out[0]
